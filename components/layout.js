bf33006fb5f90cc8023529aaead71bad1b6ae2df
Fernando Sanches <ferbs89@gmail.com>
2021-02-09 22:31:31 -0300

    - Validar usuário logado ao exibir o layout

--- components/layout.js
+++ components/layout.js
@@ -1,22 +1,29 @@
 import Head from 'next/head';
+import useUser from '../hooks/useUser';
 
 import Header from './header';
 import Menu from './menu';
 
 export default function Layout({ children, title }) {
+	const { user } = useUser();
+
 	return (
-		<div className="container">
+		<>
 			<Head>
 				<title>{title} | Ferbsystem</title>
 				<link rel="icon" href="/favicon.ico" />
 				<meta name="theme-color" content="#17496E" />
 			</Head>
 
-			<Header />
+			{user?.isLoggedIn && (
+				<div className="container">
+					<Header />
 
-			<Menu />
+					<Menu />
 
-			{children}
-		</div>
+					{children}
+				</div>
+			)}
+		</>
 	)
 }
